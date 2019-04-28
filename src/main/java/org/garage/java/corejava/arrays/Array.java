5ea06efc26c0f12d80d6513295570a133f636d41
Satish Nayak <saishkumarNayak@gmail.com>
2019-04-28 08:51:07 +0530

    Core Java - Arrays

---
+++ src/main/java/org/garage/java/corejava/arrays/Array.java
@@ -0,0 +1,40 @@
+package org.garage.java.corejava.arrays;
+
+public class Array {
+
+	int[] a; // array declare
+	int[] a1 = { 1, 2, 3 }; // array declare and initialisation
+	String[] strArray = { "How", "are", "you", "doing" };
+	String[] strArray2 = new String[3];
+
+	public void arrayIntitalWithDefaultValue() {
+		a = new int[3];
+		for (int i : a) {
+			System.out.println(i);
+		}
+	}
+
+	public void arrayIntitalWithValue() {
+
+		for (int i : a1) {
+			System.out.println(i);
+		}
+	}
+
+	public void stringArrayIntitalWithValue() {
+
+		for (String s : strArray) {
+			System.out.println(s);
+
+		}
+	}
+
+	public void stringArrayIntitalWithDefaultValue() {
+
+		for (String s : strArray2) {
+			System.out.println(s);
+
+		}
+	}
+
+}
---
+++ src/test/java/org/garage/java/corejava/ArrayTest.java
@@ -0,0 +1,38 @@
+package org.garage.java.corejava;
+
+import static org.junit.Assert.*;
+
+import org.garage.java.corejava.arrays.Array;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ArrayTest {
+
+	Array array;
+
+	@Before
+	public void setup() {
+		array = new Array();
+	}
+
+	@Test
+	public void arrayIntitalWithDefaultValueTest() {
+		array.arrayIntitalWithDefaultValue();
+	}
+
+	@Test
+	public void arrayIntitalWithValue() {
+		array.arrayIntitalWithValue();
+	}
+
+	@Test
+	public void stringArrayIntitalWithValue() {
+		array.stringArrayIntitalWithValue();
+	}
+
+	@Test
+	public void stringArrayIntitalWithDefaultValue() {
+		array.stringArrayIntitalWithDefaultValue();
+	}
+
+}
