e8ff39013d06e0bd244670427fece08c6dcacc1c
Satish Nayak <saishkumarNayak@gmail.com>
2019-04-28 08:23:30 +0530

    Core Java initial setup

---
+++ src/test/java/org/garage/java/corejava/TestOne.java
@@ -0,0 +1,14 @@
+package org.garage.java.corejava;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestOne {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
