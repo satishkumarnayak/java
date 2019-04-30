cdd18f59379c89d7c8c16695134e7d5a7b32bd7e
Satish Nayak <saishkumarNayak@gmail.com>
2019-04-30 10:58:41 +0530

    Instance/static/constructor block

--- src/main/java/org/garage/java/corejava/declareaccess/DeclareAccess.java
+++ src/main/java/org/garage/java/corejava/declareaccess/DeclareAccess.java
@@ -5,6 +5,22 @@ import static java.lang.Math.*;
 public class DeclareAccess {
 
 	int parent = 12;
+	final int instance;
+
+	{
+		System.out.println("instance block");
+		instance = 10;
+	}
+
+	static {
+		System.out.println("static block");
+	}
+
+	public DeclareAccess() {
+		// instance = 10;
+		System.out.println("constructor");
+	}
+
 	public int staticImports(int input) {
 		return (int) sqrt(input);
 
@@ -14,6 +30,7 @@ public class DeclareAccess {
 		return (int) Math.sqrt(input);
 
 	}
+
 	public void mthree() {
 		System.out.println("mthree");
 	}
