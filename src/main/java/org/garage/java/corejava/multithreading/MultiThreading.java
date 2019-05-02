4fb917457e26ffe1cd9e485a787f20cc11a2efb6
Satish Nayak <saishkumarNayak@gmail.com>
2019-05-02 11:51:41 +0530

    Threading updated

--- src/main/java/org/garage/java/corejava/multithreading/MultiThreading.java
+++ src/main/java/org/garage/java/corejava/multithreading/MultiThreading.java
@@ -16,11 +16,42 @@ class MyThread extends Thread {
 }
 
 class MyRunnable implements Runnable {
-	
+
 	public void run() {
 		for (int i = 0; i < 10; i++) {
 			System.out.println("Child Thread");
 		}
 	}
-	
+
+}
+
+class MyThreadYield extends Thread {
+
+	public void run() {
+
+		for (int i = 0; i < 10; i++) {
+			Thread.yield(); 
+			System.out.println("Child Thread");
+		}
+	}
+
+}
+
+class MyThreadSleep extends Thread {
+
+	public void run() {
+		System.out.println("Child Thread");
+	//	for (int i = 0; i < 10; i++) {
+			try {
+				Thread.sleep(100);
+				System.out.println("Child Thread");
+				
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("Child Thread");
+	//	}
+	}
+
 }
--- src/test/java/org/garage/java/corejava/multithreading/MultiThreadingTest.java
+++ src/test/java/org/garage/java/corejava/multithreading/MultiThreadingTest.java
@@ -13,16 +13,84 @@ public class MultiThreadingTest {
 			System.out.println("test main thread");
 		}
 	}
-	
-	@Test
+
+//	@Test
 	public void threadByRunnable() {
 
 		MyThread t1 = new MyThread();
 		Thread t = new Thread(new MyRunnable());
-	//	Thread t = new Thread(t1);
+		// Thread t = new Thread(t1);
 		t.start();
 		for (int i = 0; i < 10; i++) {
 			System.out.println("test main thread");
 		}
 	}
+
+	// to pause current executing Thread for giving the chance of remaining waiting
+	// Threads of same priority
+//	@Test
+	public void threadByThreadYield() {
+
+		MyThreadYield t = new MyThreadYield();
+		t.start();
+		for (int i = 0; i < 10; i++) {
+			System.out.println("test main thread");
+		}
+	}
+
+	// @Test
+	public void threadByThreadJoin() throws InterruptedException {
+
+		MyThread t = new MyThread();
+		t.start();
+		t.join();
+		for (int i = 0; i < 10; i++) {
+			System.out.println("test main thread");
+		}
+	}
+
+	/*
+	 * If a Thread wants to wait until completing some other Thread then we should
+	 * go for join() method. Example: If a Thread t1 executes t2.join() then t1
+	 * should go for waiting state until completing t2.
+	 */
+	// @Test
+	public void threadByThreadJoinTime() throws InterruptedException {
+
+		MyThread t = new MyThread();
+		t.start();
+		t.join(1);
+		for (int i = 0; i < 10; i++) {
+			System.out.println("test main thread");
+		}
+	}
+
+	/*
+	 * If a Thread don't want to perform any operation for a particular amount of
+	 * time then we should go for sleep() method.
+	 */
+	@Test
+	public void threadByThreadSleep() {
+
+		MyThreadSleep t = new MyThreadSleep();
+		t.start();
+
+		System.out.println("test main thread");
+
+	}
+
+	/*
+	 * If a Thread can interrupt a sleeping or waiting Thread by using
+	 * interrupt()(break off) method of Thread class.
+	 */
+	@Test
+	public void threadByThreadInterrupt() {
+
+		MyThreadSleep t = new MyThreadSleep();
+		t.start();
+		t.interrupt();
+
+		System.out.println("test main thread");
+	}
+
 }
